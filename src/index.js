57d2b68464f413e53f08f4617666630e7842deec
krzys1ek <combat100@gmail.com>
2017-04-09 16:44:40 +0200

    search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, (Component) from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY='AIzaSyCy8_2Lrtf4YGJkr5UABcNUnnQjNdeaw9c';
+
+
 // Create new component. This component should produce
 // some HTML
 
-const App = () => {
-  return <div>Hi! React</div>;
+const App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos});
+    });
+  }
+    render(){
+      return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 
