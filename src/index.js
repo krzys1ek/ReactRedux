c5cb28f9cbefdf419bc87f0044b839ddaae5fc92
krzys1ek <combat100@gmail.com>
2017-04-10 21:03:43 +0200

    list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React, (Component) from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY='AIzaSyCy8_2Lrtf4YGJkr5UABcNUnnQjNdeaw9c';
 
 
 // Create new component. This component should produce
 // some HTML
 
-const App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = { videos: [] };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos: videos});
+      this.setState({ videos });
     });
   }
     render(){
       return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
